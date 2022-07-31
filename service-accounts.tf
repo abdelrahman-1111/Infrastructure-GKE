2fbeff4cbdb30acbef1ca41731dceeb44daefecc
abdelrahman-1111 <eng.abdelrahman.mostafa.ali@gmail.com>
2022-07-31 17:06:00 +0200

    updating hte service accounts

--- service-accounts.tf
+++ service-accounts.tf
@@ -3,16 +3,17 @@ resource "google_service_account" "k8s-service-account" {
 }
 
 resource "google_project_iam_member" "k8s-iam-member" {
-    project = "abdo-project-12345-354211"
+    project = var.project
     role    = "roles/container.admin"
     member  = "serviceAccount:${google_service_account.k8s-service-account.email}"
 }
+
 resource "google_service_account" "k8s-cluster" {
     account_id   = "k8s-cluster"
 }
 
 resource "google_project_iam_member" "cluster-iam-member" {
-    project = "abdo-project-12345-354211"
+    project = var.project
     role    = "roles/storage.objectViewer"
     member  = "serviceAccount:${google_service_account.k8s-cluster.email}"
 }
